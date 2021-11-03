4a385477a7faf9644ac312a54481a36debc0b9f0
Sergiy Bubenko <bubenko.s91@gmail.com>
2021-11-04 01:28:25 +0200

    dz2/1ol

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -6,11 +6,11 @@ class Statistics extends Component {
     const { good, neutral, bad, total, positivePercentage } = this.props;
     return (
       <div className={s.statistics}>
-        <span>Good:{good}</span>
-        <span>Neutral:{neutral}</span>
-        <span>Bad:{bad}</span>
-        <span>Total:{total}</span>
-        <span>Positive feedback:{positivePercentage}%</span>
+        <span>Good:<span className={s.statisticsValue}>{good}</span></span>
+        <span>Neutral:<span className={s.statisticsValue}>{neutral}</span></span>
+        <span>Bad:<span className={s.statisticsValue}>{bad}</span></span>
+        <span>Total:<span className={s.statisticsValue}>{total}</span></span>
+        <span>Positive feedback:<span className={s.statisticsValue}>{positivePercentage}</span>%</span>
       </div>
     );
   }
